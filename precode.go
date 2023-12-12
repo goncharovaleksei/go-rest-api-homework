65dc42341601b47ae01ffb4ad06d9719bdacb40b
goncharovaleksei <107398636+goncharovaleksei@users.noreply.github.com>
2023-12-12 14:44:20 +0300

    Add files via upload
    добавлен функционал и тесты

--- precode.go
+++ precode.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -39,17 +41,110 @@ var tasks = map[string]Task{
 	},
 }
 
-// Ниже напишите обработчики для каждого эндпоинта
-// ...
+func getAllTasks(w http.ResponseWriter, r *http.Request) {
+	response, err := json.Marshal(tasks)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	_, err = w.Write(response)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var temp_buffer bytes.Buffer
+
+	_, err := temp_buffer.ReadFrom(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var new_task Task
+	err = json.Unmarshal(temp_buffer.Bytes(), &new_task)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	tasks[new_task.ID] = new_task
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getTaskById(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	finded_task, ok := tasks[id]
+	if !ok {
+		http.Error(w, "getTaskById: task not found", http.StatusBadRequest)
+		return
+	}
+
+	response, err := json.Marshal(finded_task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	_, err = w.Write(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+func deleteTaskById(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	finded_task, ok := tasks[id]
+
+	if !ok {
+		http.Error(w, "deleteTaskById: task not found", http.StatusBadRequest)
+		return
+	}
+
+	delete(tasks, finded_task.ID)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	r := chi.NewRouter()
 
-	// здесь регистрируйте ваши обработчики
-	// ...
+	r.Get("/tasks", getAllTasks)
+	r.Post("/tasks", createTask)
+	r.Get("/tasks/{id}", getTaskById)
+	r.Delete("/tasks/{id}", deleteTaskById)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
 		return
 	}
 }
+
+//GET http://localhost:8080/tasks
+
+//POST http://localhost:8080/tasks Body: {
+//	"ID":          "3",
+//	"Description": "3Протестировать финальное задание с помощью Postmen",
+//	"Note":        "3Лучше это делать в процессе разработки, каждый раз, когда запускаешь сервер и проверяешь хендлер",
+//	"Applications": ["The Kill","A Beautiful Lie","Attack","Live Like A Dream"]
+//}
+
+//GET http://127.0.0.1:8080/tasks/2
+
+//DEL http://localhost:8080/tasks/1
